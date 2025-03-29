c8396969a97c8e2d925c891efbaf176fc17d1378
Trainer <admin@example.com>
2025-03-29 17:55:14 +0000

    Vote HTML using DOM

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+var usElectionVotingResults = [
+    {
+        serialNo: 1,
+        region: "Florida",
+        democraticParty: 432,
+        republicanParty: 342
+    },
+    {
+        serialNo: 2,
+        region: "Arizona",
+        democraticParty: 432,
+        republicanParty: 342
+    }
+    // ......
+];
+
+// DOM Document Object Model
+
+function createVoteTable(){
+
+    var tableContainer = document.getElementById("table-container")
+
+    var tableHTML = `<table>`;
+    
+
+    usElectionVotingResults.forEach(function(item){
+        tableHTML += `<tr>
+            <td>${item.serialNo}</td>
+            <td>${item.region}</td>
+            <td>${item.democraticParty}</td>
+            <td>${item.republicanParty}</td>
+        </tr>`
+    })
+    
+    tableHTML += `</table>`
+
+    tableContainer.innerHTML = tableHTML
+}
+
+createVoteTable();
